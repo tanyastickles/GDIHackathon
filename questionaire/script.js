d0140295b935621cf6b90613a37ee74321a80a95
Tanya Stickles <tstickle@oberlin.edu>
2017-11-20 22:36:17 -0500

    removed API client_ID and client_secret from questionaire/script.js

--- questionaire/script.js
+++ questionaire/script.js
@@ -54,8 +54,8 @@ $(document).ready(function(){
 
 
   // foursquare id
-  client_id = "445SRQTN3VD4FO5GJ43TXGPEL4Q3TS0NIETFS2GH5R0KQC5Q";
-  client_secret = "TF4XDDHJCKGILSP5FWU5FUELOEP045RPOY4NI5CJ344XCZ0Q";
+  client_id = YOUR-CLIENT-ID;
+  client_secret = YOUR-CLIENT-SECRET;
 
   let foursquareUrl = 'https://api.foursquare.com/v2/venues/explore?ll=' + '42.37' + ',' + '-71.10' +
                       '&limit=30&client_id='+client_id+'&client_secret='+client_secret+"&v=20170801";
